745e1aece723a8fad63adba8bca36036c84d8477
nuenuey <162126911+nuenuey@users.noreply.github.com>
2024-09-12 11:03:05 +0900

    Add files via upload
    2주차 강의자료

--- lec02/gugudan_api.py
+++ lec02/gugudan_api.py
@@ -4,10 +4,32 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello World!'
+    html_str = """
+    
+    
+    <!DOCTYPE html>
+    <html lang = "kr">
+    <head>
+    <meta charset = "UTF-8">
+    <title> Flask Home Page </title>
+    </head>
+    <body>
+    <form method = "GET" action = "/gugudan" >
+    <h2 > 구구단 출력하기 </h2>
+    <label> 단:
+    <input type = "text" name = "dan">
+    </label >
+    <button type = "submit" > 출력 </button >
+    </form >
+    <div id="result"></div>
+    </body >
+    </html >
+        
+    """
 
+    return html_str
 
 @app.route("/hello/<name>")
 def say_hello(name):
@@ -28,5 +50,9 @@ def gugudan_arg_html():
     for j in range(1, 10):
         html_str += f"{dan} X {j}= <strong>{int(dan) * j}</strong><br>"
     return html_str
+
+
+
+
 
 app.run(debug=True)
